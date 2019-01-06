ba0641a619e5295525c370f632c61156c2d73d06
Ricardo <ricardo.belfor@ecmdeveloper.com>
2019-01-06 17:02:39 +0100

    Added inspect menu to widgets

--- js/panel-ui.js
+++ js/panel-ui.js
@@ -10,23 +10,26 @@ function getPages() {
         "$$('.icmPage').map( page => { let dijitPage = dijit.byId(page.id); console.dir(dijitPage); return { title: dijitPage.title, module: dijitPage.moduleName, id: page.id }; } );",
         function(icmPages, isException) {
             if (isException)
-            console.log("the page is not using dojo");
+                console.log("the page is not using dojo");
             else {
+
+                const table = $("#items tbody" );
+                table.empty();
+
                 for (each in icmPages) {
-                    pagesAccordion.append(`<h3>${icmPages[each].title} (${icmPages[each].module})</h3><div id='info_${icmPages[each].id}'></div>`);
+
+                    var row = `<tr id="row_${icmPages[each].id}">
+                                <td width="90%" class="ui-button" style="text-align: left" data-jq-dropdown="#jq-dropdown-1" id="${icmPages[each].id}">
+                                    <span class="ui-icon ui-icon-document"/>
+                                    <strong>${icmPages[each].title} (${icmPages[each].module}<strong>
+                                    <span style="float:right" class="ui-icon  ui-icon-triangle-1-s"></span>
+                                </td>
+                            </tr>`;
+
+                    table.append(row);
+    
                     getPageWidgets(icmPages[each].id);
                     console.log(icmPages[each]);
-                }
-                //pagesAccordion.accordion("refresh");
-
-                var button;
-
-                for (each in icmPages ) {
-                // <div id='page_${icmPages[each].id}'>Page info: </div>    
-                //     button = $(document.createElement('button'));
-                //     button.text("Widgets")
-                //     button.click({param1: icmPages[each].id}, getPageWidgets);
-                //     $( `#page_${icmPages[each].id}`).append(button);
                 }
         }
     });
@@ -36,6 +39,7 @@ function refreshPageWidgets(event) {
     console.log(event.data.param1);
     getPageWidgets(event.data.param1);
 }
+
 function getPageWidgets(pageId) {
     chrome.devtools.inspectedWindow.eval(
         `$$('.icmPageWidget', document.getElementById('${pageId}')).map( widget => widget.id )`,
@@ -45,11 +49,21 @@ function getPageWidgets(pageId) {
         else {
             var info;
 
+            const table = $("#items tbody" );
+
             for (each in pageWidgets) {
                 console.log(pageWidgets[each]);
                 // info = $(document.createElement('p'));
                 // info.html(pageWidgets[each]);
-                $(`#info_${pageId}`).append(pageWidgets[each] + "<br>");
+                //$(`#info_${pageId}`).append(pageWidgets[each] + "<br>");
+            var row = `<tr>
+                        <td width="90%" class="ui-button" style="text-align: left" data-jq-dropdown="#jq-dropdown-1" id="${pageWidgets[each]}">
+                            <span class="ui-icon ui-icon-blank"></span>${pageWidgets[each]}
+                            <span style="float:right" class="ui-icon  ui-icon-triangle-1-s"></span>
+                        </td>
+                    </tr>`;
+
+                $(`#row_${pageId}`).after(row);
             }
         }
     });
@@ -59,5 +73,20 @@ $(document).ready(function() {
 
     $('#loadPages').click(getPages);
     $("#accordion").accordion();
+    $( "#menu1" ).menu();
+    $( "#menu2" ).menu();
 
+    $("#first-item").on("click", () => { 
+        console.log(menuContext);
+        chrome.devtools.inspectedWindow.eval(`inspect(document.getElementById("${menuContext}"));`);
+    });
+
+    var menuContext;
+
+    $('#jq-dropdown-1').on('show', function(event, dropdownData) {
+        menuContext = dropdownData.trigger[0].id;
+        console.log(dropdownData);
+    }).on('hide', function(event, dropdownData) {
+        console.log(dropdownData);
+    });
 });
