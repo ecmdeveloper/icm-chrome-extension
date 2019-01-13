35738a8d59435bf7db109cb2732db56076155a57
Ricardo <ricardo.belfor@ecmdeveloper.com>
2019-01-13 07:01:12 +0100

    Only add panels if Dojo is detected

--- devtools.js
+++ devtools.js
@@ -1,24 +1,25 @@
-chrome.devtools.panels.create('Case Manager', 'images/get_started32.png', 'panel.html', function(panel){
+chrome.devtools.inspectedWindow.eval( "!(typeof dojo === 'undefined' || dojo === null )", (hasDojo, isException ) => {
 
-    console.log(panel);
-    // panel.createSidebarPane("My Sidebar",
-    // function(sidebar) {
-    //     // sidebar initialization code here
-    //     sidebar.setObject({ some_data: "Some data to show" });
-    // });
-});
+    if ( isException || !hasDojo ) {
+        return;
+    }
 
-chrome.devtools.panels.elements.createSidebarPane("Widget Properties",
-     function(sidebar) {
+    chrome.devtools.panels.create('Case Manager', 'images/get_started32.png', 'panel.html', function(panel){
+        console.log(panel);
+    });
 
-        chrome.devtools.panels.elements.onSelectionChanged.addListener(function() {
+    chrome.devtools.panels.elements.createSidebarPane("Widget Properties",
+        function(sidebar) {
 
-            chrome.devtools.inspectedWindow.eval( "if ($0.id && dijit.byId($0.id)) dijit.byId($0.id).widgetProperties", function(widgetProperties, isException) {
-                if ( isException ) {
-                    sidebar.setObject(isException);
-                } else {
-                    sidebar.setObject(widgetProperties);
-                }
-            });
-      });
+            chrome.devtools.panels.elements.onSelectionChanged.addListener(function() {
+
+                chrome.devtools.inspectedWindow.eval( "if ($0.id && dijit.byId($0.id)) dijit.byId($0.id).widgetProperties", function(widgetProperties, isException) {
+                    if ( isException ) {
+                        sidebar.setObject(isException);
+                    } else {
+                        sidebar.setObject(widgetProperties);
+                    }
+                });
+        });
+    });
 });
